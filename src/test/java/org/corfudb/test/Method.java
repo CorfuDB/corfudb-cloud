a7568bfa8b3624166c8c13261544b4af5dcf6c5e
Lin Dong <lindongchn@gmail.com>
2020-01-14 17:24:55 -0800

    logging tests to different files

---
+++ src/test/java/org/corfudb/test/Method.java
@@ -0,0 +1,31 @@
+package org.corfudb.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class Method {
+    @BeforeAll
+    public static void testSetUp() throws Exception {
+        TestLogHelper.startTestLogging("Method");
+    }
+
+    @AfterAll
+    public static void testCleanUp() {
+        TestLogHelper.stopTestLogging();
+    }
+
+    @Test
+    public void test1() {
+        log.info("test1");
+    }
+
+    @Test
+    public void test2() {
+        log.error("test2");
+    }
+}
+
---
+++ src/test/java/org/corfudb/test/TestLogHelper.java
@@ -0,0 +1,38 @@
+package org.corfudb.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+/**
+ * Class to handle setting/removing MDC on per test case basis. This helps
+ * us log each test case into it's own log file. Please see
+ * {@link http://logback.qos.ch/manual/appenders.html#SiftingAppender}
+ * and {@link http://logback.qos.ch/manual/mdc.html}
+ * @author nullin
+ */
+public class TestLogHelper
+{
+    private static final Logger log = LoggerFactory.getLogger(TestLogHelper.class);
+    public static final String TEST_NAME = "testname";
+
+    /**
+     * Adds the test name to MDC so that sift appender can use it and log the new
+     * log events to a different file
+     * @param name name of the new log file
+     * @throws Exception
+     */
+    public static void startTestLogging(String name) throws Exception {
+        MDC.put(TEST_NAME, name);
+    }
+
+    /**
+     * Removes the key (log file name) from MDC
+     * @return name of the log file, if one existed in MDC
+     */
+    public static String stopTestLogging() {
+        String name = MDC.get(TEST_NAME);
+        MDC.remove(TEST_NAME);
+        return name;
+    }
+}
