930d0659dc827eff02300514b58ecab7d0c60b68
Lin Dong <lindongchn@gmail.com>
2020-01-15 15:37:57 -0800

    solving style issues

--- src/test/java/org/corfudb/test/TestLogHelper.java
+++ src/test/java/org/corfudb/test/TestLogHelper.java
@@ -1,37 +1,30 @@
 package org.corfudb.test;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.sun.istack.internal.NotNull;
 import org.slf4j.MDC;
 
 /**
  * Class to handle setting/removing MDC on per test case basis. This helps
- * us log each test case into it's own log file. Please see
- * {@link http://logback.qos.ch/manual/appenders.html#SiftingAppender}
- * and {@link http://logback.qos.ch/manual/mdc.html}
- * @author nullin
+ * us log each test case into it's own log file.
  */
-public class TestLogHelper
-{
+public class TestLogHelper {
     public static final String TEST_NAME = "testName";
 
     /**
      * Adds the test name to MDC so that sift appender can use it and log the new
      * log events to a different file
-     * @param name name of the new log file
-     * @throws Exception
+     *
+     * @param name Class name of the test log
      */
+    @NotNull
     public static void startTestLogging(Class<?> name) {
         MDC.put(TEST_NAME, name.getCanonicalName());
     }
 
     /**
      * Removes the key (log file name) from MDC
-     * @return name of the log file, if one existed in MDC
      */
-    public static String stopTestLogging() {
-        String name = MDC.get(TEST_NAME);
+    public static void stopTestLogging() {
         MDC.remove(TEST_NAME);
-        return name;
     }
 }
