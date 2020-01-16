780cef94e2496efd3fb5cf27a2a334291b853b1a
Lin Dong <lindongchn@gmail.com>
2020-01-16 11:02:06 -0800

    move TestLogHelper to src/main/java/org.corfudb.universe.test, add reference link etc

--- src/main/java/org/corfudb/universe/test/TestLogHelper.java
+++ src/main/java/org/corfudb/universe/test/TestLogHelper.java
@@ -1,13 +1,15 @@
-package org.corfudb.test;
+package org.corfudb.universe.test;
 
 import org.slf4j.MDC;
 
 /**
  * Class to handle setting/removing MDC on per test case basis. This helps
  * us log each test case into it's own log file.
+ * See {@link <a href="http://www.nullin.com/2010/07/28/logging-tests-to-separate-files/">
+ *      Logging tests to separate files</a>}.
  */
 public class TestLogHelper {
-    public static final String TEST_NAME = "testName";
+    private static final String TEST_NAME = "testName";
 
     /**
      * Adds the test name to MDC so that sift appender can use it and log the new
