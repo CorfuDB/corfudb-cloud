ded69720ab851ed22c88ae62888e0b3e44173ef1
Viacheslav <xrw.null@gmail.com>
2020-02-19 15:33:39 -0800

    Fix bat tests: NodeDownAndLinkFailureTest

--- .ci/bat_tests.sh
+++ .ci/bat_tests.sh
@@ -2,9 +2,12 @@
 
 set -e
 
+# Clean previous state
+./gradlew -p tests clean shutdown
+
+# Deploy a new cluster, run tests and shutdown the cluster
 ./gradlew -p tests \
   clean \
   deployment \
   test -Dtags=bat \
   shutdown
-
