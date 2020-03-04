10b986dcec9bd8127f995e476cfd6d1617c29cfd
Viacheslav <xrw.null@gmail.com>
2020-03-04 14:55:36 -0800

    Moving universe framework from CorfuDb to this repository (#43)
    * Moving universe framework from CorfuDb to corfudb-test repository

---
+++ universe/src/main/java/org/corfudb/universe/universe/UniverseParams.java
@@ -0,0 +1,76 @@
+package org.corfudb.universe.universe;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSortedMap;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Builder.Default;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.ToString;
+import org.corfudb.universe.group.Group.GroupParams;
+
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+@Builder(toBuilder = true, builderMethodName = "universeBuilder")
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class UniverseParams {
+    private static final String NETWORK_PREFIX = "CorfuNet-";
+
+    @Getter
+    @Default
+    @NonNull
+    private final String networkName = NETWORK_PREFIX + UUID.randomUUID().toString();
+
+    @Default
+    @NonNull
+    private final ConcurrentMap<String, GroupParams> groups = new ConcurrentHashMap<>();
+
+    @Getter
+    @Default
+    private final boolean cleanUpEnabled = true;
+
+    /**
+     * Returns the configuration of a particular service by the name
+     *
+     * @param name group name
+     * @return an instance of {@link GroupParams} representing particular type of a group
+     */
+    public <T extends GroupParams> T getGroupParams(String name, Class<T> groupType) {
+        return groupType.cast(groups.get(name));
+    }
+
+    public UniverseParams add(GroupParams groupParams) {
+        groups.put(groupParams.getName(), groupParams);
+        return this;
+    }
+
+    /**
+     * Get all groups in the cluster
+     *
+     * @return a map of groups where key is a group name and value is the group
+     */
+    public ImmutableMap<String, GroupParams> getGroups() {
+        return ImmutableMap.copyOf(groups);
+    }
+
+    /**
+     * Finds a group by index
+     *
+     * @param index index
+     * @return group params
+     */
+    public GroupParams getGroupParamByIndex(int index) {
+        return ImmutableSortedMap.copyOf(groups)
+                .values()
+                .stream()
+                .skip(index)
+                .findFirst()
+                .orElseThrow(() -> new UniverseException("Group not found. Index: " + index));
+    }
+}
---
+++ universe/src/main/java/org/corfudb/universe/universe/vm/VmUniverseParams.java
@@ -0,0 +1,119 @@
+package org.corfudb.universe.universe.vm;
+
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.ToString;
+import org.corfudb.universe.node.server.vm.VmCorfuServerParams.VmName;
+import org.corfudb.universe.universe.Universe;
+import org.corfudb.universe.universe.UniverseParams;
+import org.corfudb.universe.util.IpAddress;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+/**
+ * Represents the parameters for constructing a VM {@link Universe}.
+ */
+@Getter
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class VmUniverseParams extends UniverseParams {
+
+    /**
+     * Default https://10.173.65.98/sdk
+     */
+    @NonNull
+    private final String vsphereUrl;
+
+    /**
+     * Default "10.172.208.208"
+     */
+    @NonNull
+    private final List<String> vsphereHost;
+
+    @NonNull
+    private final VmCredentialsParams credentials;
+
+    @NonNull
+    private final String templateVmName;
+
+    @NonNull
+    private final ConcurrentMap<VmName, IpAddress> vmIpAddresses;
+    @NonNull
+    private final String domainName = "eng.vmware.com";
+    @NonNull
+    private final String timeZone = "America/Los_Angeles";
+    @NonNull
+    private final String[] dnsServers = new String[]{"10.172.40.1", "10.172.40.2"};
+    @NonNull
+    private final String[] domainSuffixes = new String[]{"eng.vmware.com", "vmware.com"};
+    @NonNull
+    private final String[] gateways = new String[]{"10.172.211.253"};
+    @NonNull
+    private final String subnet = "255.255.255.0";
+    @NonNull
+    private final Duration readinessTimeout = Duration.ofSeconds(3);
+
+    /**
+     * Builds vm universe params object
+     *
+     * @param credentials    credentials
+     * @param vsphereUrl     vsphere url
+     * @param vsphereHost    vsphere host
+     * @param templateVmName template vm
+     * @param vmIpAddresses  vm ip address
+     * @param networkName    network name
+     * @param cleanUpEnabled is clean up enabled
+     */
+    @Builder
+    public VmUniverseParams(
+            VmCredentialsParams credentials, String vsphereUrl, List<String> vsphereHost,
+            String templateVmName, ConcurrentMap<VmName, IpAddress> vmIpAddresses, String networkName,
+            boolean cleanUpEnabled) {
+        super(networkName, new ConcurrentHashMap<>(), cleanUpEnabled);
+        this.vsphereUrl = vsphereUrl;
+        this.vsphereHost = vsphereHost;
+        this.templateVmName = templateVmName;
+        this.vmIpAddresses = vmIpAddresses;
+        this.credentials = credentials;
+    }
+
+
+    public VmUniverseParams updateIpAddress(VmName vmName, IpAddress ipAddress) {
+        vmIpAddresses.put(vmName, ipAddress);
+        return this;
+    }
+
+    /**
+     * VM credentials stored in a property file (vm.credentials.properties)
+     * The file format:
+     * <pre>
+     * vsphere.username=vSphereUser
+     * vsphere.password=vSpherePassword
+     *
+     * vm.username=vmPass
+     * vm.password=vmUser
+     * </pre>
+     */
+    @Builder
+    @Getter
+    public static class VmCredentialsParams {
+        @NonNull
+        private final Credentials vmCredentials;
+        @NonNull
+        private final Credentials vsphereCredentials;
+    }
+
+    @Builder
+    @Getter
+    public static class Credentials {
+        @NonNull
+        private final String username;
+        @NonNull
+        private final String password;
+    }
+}
