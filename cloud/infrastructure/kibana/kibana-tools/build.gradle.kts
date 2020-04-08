7cbce84857095984da670f7cef6d9cf59d7ddf23
Viacheslav <xrw.null@gmail.com>
2020-04-07 22:38:00 -0700

    Kibana-tools dockerizing

--- cloud/infrastructure/kibana/kibana-tools/build.gradle.kts
+++ cloud/infrastructure/kibana/kibana-tools/build.gradle.kts
@@ -1,12 +1,17 @@
+import java.nio.charset.StandardCharsets
+
 plugins {
     kotlin("jvm") version "1.3.71"
     kotlin("plugin.serialization") version "1.3.71"
+    id("com.palantir.docker") version "0.25.0"
 }
 
-apply(from = "${rootDir}/gradle/dependencies.gradle")
-apply(from = "${rootDir}/gradle/idea.gradle")
+apply {
+    from("${rootDir}/gradle/dependencies.gradle")
+    from("${rootDir}/gradle/idea.gradle")
+}
 
-val ktorVersion= project.extra.get("ktorVersion")
+val ktorVersion = project.extra.get("ktorVersion")
 
 dependencies {
     implementation(kotlin("stdlib"))
@@ -28,10 +33,55 @@ dependencies {
     implementation("org.apache.commons:commons-lang3:3.10")
 }
 
-sourceSets{
+sourceSets {
     main {
         resources {
             srcDir("../dashboard")
         }
     }
+}
+
+version = project.file("version")
+        .readText(StandardCharsets.UTF_8)
+        .trim()
+        .substring("version=".length)
+
+tasks.withType<Jar> {
+    archiveFileName.set("${project.name}.${archiveExtension.get()}")
+    manifest {
+        attributes["Main-Class"] = "org.corfudb.cloud.infrastructure.kibana.tools.MainKt"
+    }
+}
+
+tasks.dockerPrepare {
+    dependsOn(tasks.jar)
+
+    doLast {
+        project.copy {
+            from(configurations.runtimeClasspath.get())
+            into("$buildDir/docker/lib")
+        }
+
+        project.copy {
+            from(tasks.jar.get() as CopySpec)
+            into("$buildDir/docker/app/")
+        }
+    }
+}
+
+tasks.dockerPush {
+    dependsOn(tasks.check)
+}
+
+tasks.build {
+    dependsOn(tasks.dockerPush)
+}
+
+docker {
+    name = "corfudb/${project.name}:$version"
+    setDockerfile(file("Dockerfile"))
+}
+
+tasks.create("version").doLast {
+    println(version)
 }
