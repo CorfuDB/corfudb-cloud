0d9c0f9042e566969a6f2202789dfcd0624aebce
Viacheslav <xrw.null@gmail.com>
2020-04-27 14:15:01 -0700

    Integration-server: implement react module

--- cloud/infrastructure/integration-tools/src/main/resources/static/script.js
+++ cloud/infrastructure/integration-tools/src/main/resources/static/script.js
@@ -1,36 +1,84 @@
 'use strict';
 
-class Welcome extends React.Component {
-    render() {
-        const {handleSubmit, register, errors} = ReactHookForm.useForm;
-        const onSubmit = values => {
-            console.log(values);
-        };
+function Welcome(props) {
+    const {register, control, handleSubmit} = ReactHookForm.useForm({
+        defaultValues: {
+            test: [{name: "server1", url: "http://example.com/archive.tgz"}]
+        }
+    });
+    const { fields, append, prepend, remove, swap, move, insert } = ReactHookForm.useFieldArray(
+        {
+            control,
+            name: "test"
+        }
+    );
 
-        return (
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input name="email" ref={register({
-                    required: 'Required',
-                    pattern: {
-                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                        message: "invalid email address"
-                    }
-                })}
-                />
-                {errors.email && errors.email.message}
+    const onSubmit = data => {
+        /*const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        };*/
 
-                <input
-                    name="username"
-                    ref={register({
-                        validate: value => value !== "admin" || "Nice try!"
-                    })}
-                />
-                {errors.username && errors.username.message}
+        console.log(data)
 
-                <button type="submit">Submit</button>
-            </form>
-        );
+        //fetch('/processing/' + data.aggregationUnit, requestOptions)
+          //  .then(response => response.json())
+            //.then(response => this.setState({ postId: response.id }));
     }
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <h4>Archives</h4>
+
+            <label>Aggregation unit: </label>
+            <input
+                name={"aggregationUnit"}
+                ref={register()}
+                size={30}
+            />
+            <br/>
+            <br/>
+
+            <section>
+                <button
+                    type="button"
+                    onClick={() => {
+                        append({name: "server", url: "http://example.com/arhive.tgz"});
+                    }}
+                >
+                    Append
+                </button>
+            </section>
+
+            <ul>
+                {fields.map((item, index) => {
+                    return (
+                        <li key={item.id}>
+                            <input
+                                name={'archives[${index}].name'}
+                                defaultValue={`${item.name}`} // make sure to set up defaultValue
+                                ref={register()}
+                            />
+
+                            <ReactHookForm.Controller
+                                as={<input/>}
+                                name={'archives[${index}].url'}
+                                size={70}
+                                control={control}
+                                defaultValue={item.url} // make sure to set up defaultValue
+                            />
+                            <button type="button" onClick={() => remove(index)}>
+                                Delete
+                            </button>
+                        </li>
+                    );
+                })}
+            </ul>
+
+            <input type="submit"/>
+        </form>
+    );
 }
 
 ReactDOM.render(
