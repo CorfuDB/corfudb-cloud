9eb9a402608a03618a9bef6ba071636c79b85f7a
Viacheslav <xrw.null@gmail.com>
2020-06-08 13:49:00 -0700

    Elk v 7.7.1 (#80)
    Migrate to ELK 7.7.1, change to the basic license

--- cloud/infrastructure/docker-elk/init.sh
+++ cloud/infrastructure/docker-elk/init.sh
@@ -8,7 +8,9 @@ git clone https://github.com/deviantony/docker-elk.git build/docker-elk
 docker volume rm docker-elk_elasticsearch
 
 #copy configs into a build dir
-cp docker-compose.yml build/docker-elk
-cp .env build/docker-elk
+cp -v docker-compose.yml build/docker-elk
+cp -v .env build/docker-elk
+cp -av elasticsearch build/docker-elk
+cp -av kibana build/docker-elk
 
 mkdir build/docker-elk/data
