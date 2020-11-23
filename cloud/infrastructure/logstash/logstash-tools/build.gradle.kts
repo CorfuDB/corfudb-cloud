e04c513b234736434c83a2dc8a30fa7d5c568b75
Viacheslav <xrw.null@gmail.com>
2020-11-22 23:08:50 -0800

    Github Actions: trigger cloud checks on pull requests

--- cloud/infrastructure/logstash/logstash-tools/build.gradle.kts
+++ cloud/infrastructure/logstash/logstash-tools/build.gradle.kts
@@ -6,11 +6,13 @@ apply(from = "${rootDir}/gradle/dependencies.gradle")
 apply(from = "${rootDir}/gradle/java.gradle")
 apply(from = "${rootDir}/gradle/idea.gradle")
 
+val testcontainersVersion = project.ext["testcontainersVersion"] as String
+
 dependencies {
     implementation("ch.qos.logback:logback-classic:1.2.3")
-    implementation("org.testcontainers:testcontainers:1.13.0")
+    implementation("org.testcontainers:testcontainers:${testcontainersVersion}")
 
-    testImplementation("org.testcontainers:junit-jupiter:1.13.0")
+    testImplementation("org.testcontainers:junit-jupiter:${testcontainersVersion}")
 
     compileOnly ("org.immutables:value:2.8.2")
     annotationProcessor("org.immutables:value:2.8.2")
