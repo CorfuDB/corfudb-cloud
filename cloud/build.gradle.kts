8f1c2011d36b20e3bdabca71b2dfa634f3630b0e
Viacheslav <314292+xnull@users.noreply.github.com>
2021-04-29 23:04:37 -0700

    Dockerized universe tests (#147)

--- cloud/build.gradle.kts
+++ cloud/build.gradle.kts
@@ -2,20 +2,5 @@ plugins {
     idea
 }
 
-idea {
-    project {
-        // Set the version control system
-        // to Git for this project.
-        // All values IntelliJ IDEA supports
-        // can be used. E.g. Subversion, Mercurial.
-        vcs = "Git"
-
-        setLanguageLevel("1.8")
-    }
-
-    module {
-        isDownloadJavadoc = true
-        isDownloadSources = false
-    }
-}
-
+val gradleScriptsDir: String = project.rootDir.parent
+apply(from="${gradleScriptsDir}/gradle/idea-project.gradle.kts")
