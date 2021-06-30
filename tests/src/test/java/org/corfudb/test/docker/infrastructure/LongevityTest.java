c767bafdb125a89e6b96d426cea5a208c97009a5
Viacheslav <314292+xnull@users.noreply.github.com>
2021-06-29 22:45:39 -0700

    Longevity test: bring failures (#169)
    * Longevity test: bring failures

--- tests/src/test/java/org/corfudb/test/docker/infrastructure/LongevityTest.java
+++ tests/src/test/java/org/corfudb/test/docker/infrastructure/LongevityTest.java
@@ -3,21 +3,28 @@ package org.corfudb.test.docker.infrastructure;
 import lombok.extern.slf4j.Slf4j;
 import org.corfudb.test.AbstractCorfuUniverseTest;
 import org.corfudb.test.TestGroups;
+import org.corfudb.test.failure.NodeFailure;
 import org.corfudb.universe.api.universe.group.cluster.Cluster.ClusterType;
 import org.corfudb.universe.infrastructure.docker.universe.group.cluster.DockerCorfuLongevityCluster;
 import org.corfudb.universe.infrastructure.docker.universe.node.server.DockerCorfuServer.DockerCorfuLongevityApp;
+import org.corfudb.universe.infrastructure.docker.workflow.DockerUniverseWorkflow;
 import org.corfudb.universe.scenario.fixture.UniverseFixture;
+import org.corfudb.universe.universe.group.cluster.corfu.CorfuCluster.GenericCorfuCluster;
+import org.corfudb.universe.universe.node.client.CorfuClient;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.slf4j.event.Level;
 
 import java.nio.file.Paths;
+import java.time.Duration;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
 @Slf4j
 @Tag(TestGroups.LONGEVITY_DOCKER)
 public class LongevityTest extends AbstractCorfuUniverseTest {
+
+    private final Duration testDuration = Duration.ofHours(1);
 
     @Test
     public void test() {
@@ -26,22 +33,48 @@ public class LongevityTest extends AbstractCorfuUniverseTest {
                     .enabled(true)
                     .universeDirectory(Paths.get("build"));
 
-            fixture.getLongevityApp().timeAmount(5);
+            fixture.getLongevityApp().timeAmount((int) testDuration.toMinutes());
         };
 
         testRunner.executeDockerTest(setup, wf -> {
-            DockerCorfuLongevityCluster longevityCluster = wf
-                    .getUniverse()
-                    .getGroup(ClusterType.CORFU_LONGEVITY_APP);
+            DockerCorfuLongevityCluster longevityCluster = runLongevity(wf);
 
-            longevityCluster.nodes().forEach((name, app) -> {
-                log.info("Application endpoint: {}, app params: {}", app.getEndpoint(), app.getParams());
-            });
+            executeFailures(wf);
 
             waitForLongevityFinished(longevityCluster);
-
-            //Run Injection verification
         });
+    }
+
+    private void executeFailures(DockerUniverseWorkflow wf) throws Exception {
+        long start = System.currentTimeMillis();
+
+        GenericCorfuCluster corfuCluster = wf.getUniverse().getGroup(ClusterType.CORFU);
+        CorfuClient corfuClient = corfuCluster.getLocalCorfuClient();
+
+        while (true) {
+            Duration duration = Duration.ofMillis(System.currentTimeMillis() - start);
+
+            if (duration.toMinutes() + 5 > testDuration.toMinutes()) {
+                break;
+            }
+
+            int randomServerIndex = new Random().nextInt(3);
+            NodeFailure oneNodeFailure = new NodeFailure(corfuClient, corfuCluster.getServerByIndex(randomServerIndex));
+            oneNodeFailure.failure((client, server) -> {
+
+            });
+        }
+    }
+
+    private DockerCorfuLongevityCluster runLongevity(DockerUniverseWorkflow wf) {
+        DockerCorfuLongevityCluster longevityCluster = wf
+                .getUniverse()
+                .getGroup(ClusterType.CORFU_LONGEVITY_APP);
+
+        longevityCluster.nodes().forEach((name, app) -> {
+            log.info("Application endpoint: {}, app params: {}", app.getEndpoint(), app.getParams());
+        });
+        return longevityCluster;
     }
 
     private void waitForLongevityFinished(DockerCorfuLongevityCluster longevityCluster) throws Exception {
---
+++ tests/src/test/java/org/corfudb/test/failure/NodeFailure.java
@@ -0,0 +1,78 @@
+package org.corfudb.test.failure;
+
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.corfudb.universe.api.universe.node.ApplicationServers.CorfuApplicationServer;
+import org.corfudb.universe.universe.node.client.CorfuClient;
+
+import java.time.Duration;
+import java.util.function.BiConsumer;
+
+import static org.corfudb.universe.test.util.ScenarioUtils.verifyNodeStatusIsDown;
+import static org.corfudb.universe.test.util.ScenarioUtils.waitForClusterStatusDegraded;
+import static org.corfudb.universe.test.util.ScenarioUtils.waitForClusterStatusStable;
+import static org.corfudb.universe.test.util.ScenarioUtils.waitForUnresponsiveServersChange;
+
+/**
+ * Stops a corfu server and brings it back
+ */
+@Slf4j
+@AllArgsConstructor
+public class NodeFailure {
+
+    @NonNull
+    private final CorfuClient corfuClient;
+    @NonNull
+    private final CorfuApplicationServer server;
+
+    /**
+     * Stops a corfu node, executes an action and recovers from the failure
+     *
+     * @param action action
+     * @throws Exception exception
+     */
+    public void failure(BiConsumer<CorfuClient, CorfuApplicationServer> action) throws Exception {
+        failure();
+        action.accept(corfuClient, server);
+        recover();
+    }
+
+    /**
+     * Stops a corfu node
+     *
+     * @throws Exception exception
+     */
+    public void failure() throws Exception {
+        // Stop one node and wait for layout's unresponsive servers to change
+        log.info("Stop one node");
+        server.stop(Duration.ofSeconds(10));
+
+        log.info("Verify responsive servers");
+        waitForUnresponsiveServersChange(size -> size == 1, corfuClient);
+
+        // Verify cluster status is DEGRADED with one node down
+        log.info("Verify Cluster status is Degraded...");
+        waitForClusterStatusDegraded(corfuClient);
+
+        log.info("Verify that node is down");
+        verifyNodeStatusIsDown(corfuClient, server);
+    }
+
+    /**
+     * Starts the corfu server
+     *
+     * @throws Exception exception
+     */
+    public void recover() throws Exception {
+        // restart the stopped node and wait for layout's unresponsive servers to change
+        log.info("Start the paused server now");
+        server.start();
+
+        waitForUnresponsiveServersChange(size -> size == 0, corfuClient);
+
+        // Verify cluster status is STABLE
+        log.info("Verify cluster status");
+        waitForClusterStatusStable(corfuClient);
+    }
+}
