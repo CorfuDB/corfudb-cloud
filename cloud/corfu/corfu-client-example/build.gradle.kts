70ce13554a2a4c1f0d691e5a0e8c4b13fe602e56
Viacheslav <314292+xnull@users.noreply.github.com>
2022-05-20 12:50:34 -0700

    Corfu Cloud Runtime example (#201)
    * runtime
    
    * Cloud corfu client: changes
    
    * Cloud corfu client: build
    
    Co-authored-by: Pavel Zaytsev <zaytsev.pavel.a@gmail.com>

---
+++ cloud/corfu/corfu-client-example/build.gradle.kts
@@ -0,0 +1,50 @@
+import java.nio.charset.StandardCharsets
+
+plugins {
+    java
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+
+    maven {
+        name = "GitHubPackages"
+        url = uri("https://maven.pkg.github.com/corfudb/corfudb")
+    }
+}
+
+val corfuVersion = "0.3.2-SNAPSHOT"
+val logbackVersion = "1.2.11"
+val junitVersion = "5.8.2"
+
+dependencies {
+    implementation("ch.qos.logback:logback-classic:${logbackVersion}")
+
+    implementation("org.corfudb:runtime:${corfuVersion}") {
+        exclude(group = "io.netty", module = "netty-tcnative")
+    }
+
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:${junitVersion}")
+}
+
+version = project.file("version")
+    .readText(StandardCharsets.UTF_8)
+    .trim()
+    .substring("version=".length)
+
+//Fat jar
+tasks.withType<Jar> {
+    archiveFileName.set("${project.name}.${archiveExtension.get()}")
+    manifest {
+        attributes["Main-Class"] = "org.corfudb.cloud.runtime.example.Main"
+    }
+
+    from(configurations.compileClasspath.get().map {
+        if (it.isDirectory) it else zipTree(it)
+    })
+}
+
+tasks.create("version").doLast {
+    println(version)
+}
--- cloud/settings.gradle.kts
+++
@@ -1,8 +0,0 @@
-rootProject.name = "corfu-cloud-native-ecosystem"
-
-include("infrastructure:kibana:kibana-tools")
-include("infrastructure:logstash:logstash-tools")
-include("infrastructure:integration-tools")
-include("infrastructure:filebeat:filebeat")
-include("infrastructure:logstash:logstash")
-
