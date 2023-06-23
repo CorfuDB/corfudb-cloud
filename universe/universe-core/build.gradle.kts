ac99c9aca621deeaf5b8dd6a4f86f6c0f0f06443
Viacheslav <314292+xnull@users.noreply.github.com>
2023-06-22 17:30:55 -0700

    Get rid of jfrog plugins (#245)

--- universe/universe-core/build.gradle.kts
+++ universe/universe-core/build.gradle.kts
@@ -11,7 +11,6 @@ plugins {
     id("com.github.spotbugs") version "3.0.0"
     id("jacoco")
     id("maven-publish")
-    id("com.jfrog.artifactory") version "4.18.0"
 }
 
 val gradleScriptDir = "${rootDir.parent}/gradle"
