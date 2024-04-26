0346a9e5e308eb223f1e30106d67ccd3bb8d33fc
Viacheslav <xrw.null@gmail.com>
2024-04-26 11:28:45 -0700

    lap pognali

--- cloud/infrastructure/docker-elk/init.sh
+++ cloud/infrastructure/docker-elk/init.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
+set -ex
+
 #download docker-elk repo into a build directory
 rm -rf build/docker-elk/
 
 git clone https://github.com/deviantony/docker-elk.git build/docker-elk
 
+cd build/docker-elk
+git checkout 1bee7e1c097d3c2f9d72995d5ca2d6051f29381b
+cd ../../
+
 # do not delete elastic volume
-#docker volume rm docker-elk_elasticsearch
+docker volume rm docker-elk_elasticsearch
 
 #copy configs into a build dir
 cp -v docker-compose.yml build/docker-elk
